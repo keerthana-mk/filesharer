a2194e2edae566956a150e172f9924a19f3f903f
Keerthana S <e1626285@titan.com>
2019-02-09 22:59:34 +0530

    added to copy file excel file

---
+++ src/main/java/mk/learning/fileshare/constants/HashmapConstants.java
@@ -0,0 +1,13 @@
+package mk.learning.fileshare.constants;
+
+import java.util.HashMap;
+
+public class HashmapConstants {
+	
+	public static HashMap<String,HashMap<String,String>> mapOfMaps= null;
+	
+	public static String HR_DETAILS = "hr_details";
+	
+	public static String FINANCE_DETAILS = "finance_details";
+	
+}
